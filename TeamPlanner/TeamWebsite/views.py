8991c3a074fd2fc860910e84da674c5b46dd95de
HamishB1 <123337631+HamishB1@users.noreply.github.com>
2024-05-13 23:36:12 +0000

    added "template" pathway for .html

--- TeamPlanner/TeamWebsite/views.py
+++ TeamPlanner/TeamWebsite/views.py
@@ -6,14 +6,14 @@ from .models import Team, Member
 def home(request):
     teams = Team.objects.all()
     members = Member.objects.all()
-    return render(request, 'home.html', {'teams': teams, 'members': members})
+    return render(request, 'templates/home.html', {'teams': teams, 'members': members})
 
 def team_detail(request, team_id):
     team = get_object_or_404(Team, id=team_id)
-    return render(request, 'team_detail.html', {'team': team})
+    return render(request, 'teamplates/team_detail.html', {'team': team})
 
 def member_detail(request, member_id):
     member = get_object_or_404(Member, id=member_id)
-    return render(request, 'member_detail.html', {'member': member})
+    return render(request, 'templates/member_detail.html', {'member': member})
 
 # Add more view functions for CRUD operations
